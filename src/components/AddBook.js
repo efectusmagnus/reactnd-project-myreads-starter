031a35547216e2d01f2d9dd222104bb5fac680be
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-11-29 09:09:56 +0100

    Add AddBook.js (v2)

---
+++ src/components/AddBook.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+// Using a stateless functional component because we are only rendering
+const AddBook = (props) => {
+  const {currentBooks} = props
+  return(
+    <div className="open-search">
+      <Link to={{
+        pathname: '/search',
+        state: {
+          booksFromStartPage: currentBooks
+        }
+      }} />
+    </div>{/* End of open search */}
+  )
+}
+
+//Use PropTypes to typecheck the props for a component
+AddBook.PropTypes = {
+  currentBooks: PropTypes.array.isRequired
+}
+
+export default AddBook
