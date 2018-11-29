b678584aaa7cacfda725602752d04bc72d0637f1
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-11-29 10:10:26 +0100

    Correct code of Startpage.js

--- src/js/StartPageTitle.js
+++ src/js/StartPageTitle.js
@@ -4,12 +4,27 @@ import PropTypes from 'prop-types'
 // Using a stateless functional component, as we are only rendering
 // The title is a immuable property (that is why we use prop rather than state)
 const StartPage = (props) => {
-  <div className="list-of-books-title">
-    <h1>{props.title}</h1>
-  </div>{/* End of list of books title*/}
+  return(
+    <div className="list-books-title">
+      <h1>{props.title}</h1>
+    </div>{/* End of list books title*/}
+  )
 }
 
 // Set default prop value using the .defaultProps property
+StartPageTitle.defaultProps = {
+  title: 'MyReads'
+}
+
+// Use PropTypes to typecheck the props for a component
 StartPageTitle.PropTypes = {
   title: PropTypes.string.isRequired
 }
+
+
+// Set default prop value using the .defaultProps property
+StartPageTitle.PropTypes = {
+title: PropTypes.string.isRequired
+}
+
+export default StartPageTitle
