e0032c579eb71b8929af4c661b3b04bd4c3cae3f
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-11-29 09:18:50 +0100

    Old version of BookShelf.js (!important)

---
+++ src/components/BookShelf.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book';
+
+class BookShelf extends Component {
+  static propTypes = {
+    books: propTypes.array.isRequired,
+    changeShelf: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { books, changeShelf } = this.props;
+
+    return(
+      <ol className="books-grid">{books.map(book => (
+        <Book book={book} books={books} key={book.id} changeShelf={changeShelf} />
+        ))}
+      </ol>
+    );
+   }
+}
+
+export default BookShelf;
