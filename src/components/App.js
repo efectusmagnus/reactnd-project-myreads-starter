463040c109550216ee65a81c6b12d7aacc2a743a
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-11-29 08:47:52 +0100

    Change building strategy of App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,18 @@
-import React from 'react';
-import * as BooksAPI from './BooksAPI';
-import '../css/App.css';
-import { Route, Switch } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import BookList from './BookList';
-import BookSearcher from './BookSearcher';
-//import NoImageFound from './NoImageFound';
+import React from 'react'
+import { Route } from 'react-router-dom'
+import  StartPage from './StartPage'
+import  SearchPage from './SearchPage'
+import '../css/App.css'
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * DONE: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-     books: [],
-     currentlyReading: [],
-     wantToRead: [],
-     read: []
-  }
-  componentDidMount() {
-    this.listOfBookShelf()
-  }
+// Using a stateless functional component, as we are only rendering
 
-  listOfBookShelf() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({
-        books: books,
-        currentlyReading: books.filter((book) => book.shelf === 'currentlyReading'),
-        wantToRead: books.filter((book) => book.shelf === 'wantToRead'),
-        read: books.filter((book) => book.shelf === 'read')
-      })
-    })
-  }
-
-  changeShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf).then((books) => {
-      this.listOfBookShelf()
-    })
-  }
-  render() {
-    return (
-      <div className="app">
-        <Route exact path="/" render={() => (
-          <BookList books={this.state.books} changeShelf={this.changeShelf} />
-        )}/>
-
-        <Route path="/search" render={({ history}) => (
-          <Search books={this.state.books} changeShelf={this.changeShelf} />
-        )}/>
-
-      </div>
+const BooksApp = () => {
+  return(
+    <div className="app">
+      <Route exact path="/" component={StartPage}/>
+      <Route path="/search" component={SearchPage}/>
+    </div>
+  )
+}
 
 export default BooksApp
