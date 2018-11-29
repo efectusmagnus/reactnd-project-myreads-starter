47f6a3bf2c85472156e1d6b800c24d1157bb0667
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-11-29 09:22:10 +0100

    Add ShelfBoard.js file

---
+++ src/components/ShelfBoard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Book from './Book'
+
+// Using a stateless functional component, as we are only rendering
+const ShelfBoard = (props) => {
+  const {ShelfBoardIndex, books, onChangeShelf} = props
+  return(
+    <div>
+      <div className="bookshelf-books" key={ShelfBoardIndex}>
+        <ol className="books-grid">
+          {books.map((book) => (
+            <Book key={book.id} book={book} onChangeShelf={onChangeShelf}/>
+          ))}
+        </ol>
+      </div>{/*End of bookshelf-books */}
+    </div>{/*End of div to make react happy*/}
+  )
+}
+
+// Use PropTypes to typecheck the props for a component
+ShelfBoard.PropTypes = {
+  ShelfBoardIndex: PropTypes.number.isRequired,
+  books: PropTypes.array.isRequired,
+  onChangeShelf: PropTypes.func.isRequired
+}
+
+export default ShelfBoard
