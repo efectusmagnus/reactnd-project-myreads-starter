441316121f6d8851bdfafd011b115415b11e757b
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-02 06:08:24 +0100

    Convert div into img

--- src/js/Book.js
+++ src/js/Book.js
@@ -9,9 +9,11 @@ const Book = (props) => {
     <li>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{
-            backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail: noBookCover})`}}>
-          </div>
+          <img className="book-cover" alt="" style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail: noBookCover})`}}
+          />
           <div className="book-shelf-changer">
             <select
               onChange={(event) => onChangeShelf(book, event.target.value)}
