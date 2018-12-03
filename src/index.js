4cec1e84e09904f897d4b14c269b0c28b9ffb309
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-03 16:12:42 +0100

    Add a unregister service worker to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './js/App'
 import './css/index.css'
+import * as serviceWorker from '.serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -10,3 +11,8 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
