fd9218512cf6defa15128811ac3ea7a9ecb976f1
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-03 21:38:41 +0100

    Redirect to start page when page doesn't exist (App.js)

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import  StartPage from './StartPage'
-import  SearchPage from './SearchPage'
-import PageNotFound from '../images/owl-page-not-found.png'
+import { BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+import StartPage from './StartPage'
+import SearchPage from './SearchPage'
 import '../css/App.css'
+/*import PageNotFound from '../images/PageNotFound.png'*/
 
+const PageNotFound = ({location}) => (
+  <div>
+    <h3>No match for <code>{location.pathname}</code></h3>
+  </div>
+)
 // Using a stateless functional component, as we are only rendering
-
 const BooksApp = () => {
   return(
     <div className="app">
-      <Route exact path="/" alt="home page" component={ StartPage }/>
-      <Route path="/search" alt="search page" component={ SearchPage }/>
+      <Router>
+        <Switch>
+          <Route path="/" alt="home page" exact component={ StartPage } />
+          <Redirect to="/" />
+          <Route path="/search" alt="search page" component={ SearchPage } />
+          <Route component={ PageNotFound } />
+        </Switch>
+      </Router>
     </div>
   )
 }
