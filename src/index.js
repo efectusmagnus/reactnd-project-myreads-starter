4a4b24e588766e568b8ecdfc5414f6d6f918ea11
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-04 18:05:39 +0100

    Change path index.css to Index.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './js/App';
 import * as registerServiceWorker from './registerServiceWorker';
-import './css/index.css';
+import './css/Index.css';
 
 ReactDOM.render(
   <BrowserRouter>
