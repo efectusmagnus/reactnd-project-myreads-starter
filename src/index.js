3e0adabbd124d3b9620656f11a8ed439a5168e3a
July Natalia da Silva Delgado Habermann <efectusmagnus@gmail.com>
2018-12-04 08:45:22 +0100

    Test service worker

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './js/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as registerServiceWorker from './registerServiceWorker';
 import './css/index.css';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root');
+  document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-registerServiceWorker(); // Runs register() as default function
+registerServiceWorker.register(); // Runs register() as default function
